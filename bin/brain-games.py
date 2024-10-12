8e022e581977e9c1e19489afd8de42267c4f22c4
MDoak <165653394+MDoak@users.noreply.github.com>
2024-10-12 13:42:04 +0300

    Update brain-games.py

--- bin/brain-games.py
+++ bin/brain-games.py
@@ -1,3 +1,8 @@
+import sys
+from pathlib import Path
+# Разрешите расширить sys.path, добавив в него родительскую директорию.
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 from src.games.gcm import gameLCM
 from src.games.progression import gameProgres
 from src.engine import greetUser, playBrainGame
